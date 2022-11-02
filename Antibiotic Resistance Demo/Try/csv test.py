f23f5408d56606a117a74175c5e88bb6b6af7223
chichael-meng <96350188+chichael-meng@users.noreply.github.com>
2022-11-02 03:12:58 -0400

    Delete Antibiotic Resistance Demo directory

--- Antibiotic Resistance Demo/Try/csv test.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('bruh.csv', 'a', newline = '') as f:
-    writer = csv.writer(f)
-    header = ['time', 'num_bacteria', 'initial_num_phage', 'initial_num_antibiotic', 'mean_resistance']
-    writer.writerow(header)
-
-for x in range(1):
-    print('bruh')
